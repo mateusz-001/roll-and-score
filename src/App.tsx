45815209a4a4f02ebfbcd30cfe963e5ae55a75a1
mateusz-001 <matistopa2001@gmail.com>
2025-07-23 17:26:27 +0200

    heading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
-import viteLogo from '/vite.svg';
 import './App.css';
 
-import { useState } from 'react';
-
-import reactLogo from './assets/react.svg';
+import { Heading } from './components/Heading';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="bg-red-500">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="font-heading text-4xl font-bold">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="flex flex-col gap-4">
+      <Heading level="h1">Vite + React + TypeScript + Tailwind CSS</Heading>
+      <Heading level="h2">Vite + React + TypeScript + Tailwind CSS</Heading>
+      <Heading level="h3">Vite + React + TypeScript + Tailwind CSS</Heading>
+      <Heading level="h4">Vite + React + TypeScript + Tailwind CSS</Heading>
+      <Heading level="h5">Vite + React + TypeScript + Tailwind CSS</Heading>
+      <Heading level="h6">Vite + React + TypeScript + Tailwind CSS</Heading>
+    </div>
   );
 }
 
