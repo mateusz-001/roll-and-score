4128671fd3cd75190a491d3c79f13c410cb373fe
mateusz-001 <matistopa2001@gmail.com>
2025-10-08 21:35:28 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { paths } from './routes/paths';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-bg text-text">
+    <main className="grid grid-cols-1 min-h-screen bg-bg text-text relative md:grid-cols-[110px_1fr] lg:grid-cols-[220px_1fr]">
       <Navigation />
       <div className="max-w-3xl mx-auto px-4 py-6">
         <Routes>
@@ -19,6 +19,6 @@ export default function App() {
           <Route path={paths.hof} element={<HallOfFamePage />} />
         </Routes>
       </div>
-    </div>
+    </main>
   );
 }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,29 +1,28 @@
+import { Crown, Dices, Gamepad2, House } from 'lucide-react';
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import { Navlink } from './Navlink';
 
 export const Navigation: React.FC = () => {
   return (
-    <nav className="bg-primary text-white p-4">
-      <ul className="flex space-x-4">
+    <nav
+      className="md:bg-gradient-to-b
+      from-primary
+      via-secondary
+      md:to-accent/70 bg-primary md:bg-secondary text-white p-1 max-md:fixed max-md:bottom-0 max-md:inset-x-0 max-md:mx-auto max-md:z-50 max-md:rounded-t-md md:h-auto md:max-h-screen md:rounded-lg md:p-3 md:m-4 lg:p-4"
+    >
+      <ul className="flex max-md:justify-between max-md:w-full md:flex-col lg:gap-2.5">
         <li>
-          <Link to="/" className="hover:underline">
-            Start
-          </Link>
+          <Navlink to="/" icon={<House />} label="Home" />
         </li>
         <li>
-          <Link to="/game" className="hover:underline">
-            Game
-          </Link>
+          <Navlink to="/game" icon={<Dices />} label="Game" />
         </li>
         <li>
-          <Link to="/results" className="hover:underline">
-            Results
-          </Link>
+          <Navlink to="/results" icon={<Gamepad2 />} label="Results" />
         </li>
         <li>
-          <Link to="/hall-of-fame" className="hover:underline">
-            Hall of Fame
-          </Link>
+          <Navlink to="/hall-of-fame" icon={<Crown />} label="Hall of Fame" />
         </li>
       </ul>
     </nav>
