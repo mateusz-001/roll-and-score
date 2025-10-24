4832aaf91867e0f9ad8ddfe1694ec934418b767e
mateusz-001 <matistopa2001@gmail.com>
2025-10-24 12:39:01 +0200

    players register form

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -3,11 +3,19 @@ import React from 'react';
 import { PageWrapper } from '@/components/PageWrapper';
 
 import { Intro } from './Intro';
+import { IntroForm } from './IntroForm';
 
 export const StartPage: React.FC = () => {
+  const [showForm, setShowForm] = React.useState<boolean>(false);
+
+  const handleSetShowForm = () => setShowForm(prev => !prev);
+
   return (
-    <PageWrapper className="h-screen">
-      <Intro />
+    <PageWrapper className="min-h-screen">
+      <header className="relative pt-28 max-md:py-28">
+        {!showForm && <Intro handleSetShowForm={handleSetShowForm} />}
+        {showForm && <IntroForm handleSetShowForm={handleSetShowForm} />}
+      </header>
     </PageWrapper>
   );
 };
